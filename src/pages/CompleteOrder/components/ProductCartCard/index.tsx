e2607edccd9b1db892887663a99dfc2bc42a2c40
Leonardo Pessoa <pessoa@MacBook-Air-de-Leonardo.local>
2023-12-28 21:11:05 -0300

    change photo for img

--- src/pages/CompleteOrder/components/ProductCartCard/index.tsx
+++ src/pages/CompleteOrder/components/ProductCartCard/index.tsx
@@ -35,7 +35,7 @@ export function ProductCartCard({ product }: ProductCardCardProps) {
   return (
     <ProductCartCardContainer>
       <div>
-        <img src={`/Products/${product.photo}`} alt="Café" />
+        <img src={`/Products/${product.img}`} alt="Café" />
 
         <div>
           <RegularText color="subtitle">{product.name}</RegularText>
